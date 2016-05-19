604601d2cb6604e00a979a5be53fdd205cf1cd5e
Fatih Ayyıldız <fatih.ayyildiz@inveon.com.tr>
2016-05-19 20:10:41 +0300

    Leaflet library'si kaldırılıp denemek için cordova-google-maps plugini eklendi.

--- www/js/controllers/MapController.js
+++ www/js/controllers/MapController.js
@@ -1,53 +1,16 @@
 angular.module('Pakkage.MapController', [])
-  .controller('MapCtrl', ['$scope', '$state', 'LoadingService','leafletData', function($scope, $state, LoadingService,leafletData) {
-    angular.extend($scope, {
-        istanbul: {
-            lat: 41.045002,
-            lng: 29.001957,
-            zoom: 12
-        },
-        layers: {
+  .controller('MapCtrl', ['$scope', '$state', 'LoadingService', function($scope, $state, LoadingService) {
 
-                  baselayers: {
-                    googleRoadmap: {
-                        name: 'Google Streets',
-                        layerType: 'ROADMAP',
-                        type: 'google'
-                    },
-                      googleTerrain: {
-                          name: 'Google Terrain',
-                          layerType: 'TERRAIN',
-                          type: 'google'
-                      },
-                      googleHybrid: {
-                        name: 'Google Hybrid',
-                        layerType: 'HYBRID',
-                        type: 'google'
-                    }
+      // Define a div tag with id="map_canvas"
+      var mapDiv = document.getElementById("map_canvas");
 
-                  }
+      // Initialize the map plugin
+      var map = plugin.google.maps.Map.getMap(mapDiv);
 
+      // You have to wait the MAP_READY event.
+      map.on(plugin.google.maps.event.MAP_READY, onMapInit);
+    
 
-        },
-        markers: {
-
-            uskudar: {
-                group: 'istanbul',
-                lat: 41.011559,
-                lng: 29.052887,
-                label: {
-                    message: "<h1>Üsküdar da bir yer</h1><br /> alt satır"
-                }
-            },
-
-            umraniye: {
-                group: 'istanbul',
-                lat: 41.045738,
-                lng: 29.096907,
-                label: {
-                    message: "Ümraniyede bir yer "
-                }
-            }
-        },
-    });
+    function onMapInit(map) {
+    }
   }])
